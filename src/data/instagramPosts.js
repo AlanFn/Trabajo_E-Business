d1c5033a5587b5393b44d2def26d5ba43f77f46c
Alan Fabricio Nuñez Vera <alanfabricionunezvera@gmail.com>
2026-05-13 11:24:14 -0300

    Agregar imagenes para el carousel de instagram

--- src/data/instagramPosts.js
+++ src/data/instagramPosts.js
@@ -2,26 +2,26 @@ export const instagramPosts = [
   {
     type: "Reel",
     href: "https://www.instagram.com/vtech.py/reel/DW1oujdjk7L/",
-    image: "https://images.unsplash.com/photo-1558769132-cb1aea458c5e?w=500&q=80",
+    image: "dist/assets/image1_post.jpg",
   },
   {
     type: "Foto",
     href: "https://www.instagram.com/vtech.py/p/DWl_XgsDpPe/",
-    image: "https://images.unsplash.com/photo-1559839734-2b71ea197ec2?w=500&q=80",
-  },
-  {
-    type: "Reel",
-    href: "https://www.instagram.com/vtech.py/reel/DUZOieWD8Vl/",
-    image: "https://images.unsplash.com/photo-1623002328565-fde0de4b2b08?w=500&q=80",
+    image: "dist/assets/image2_post.jpg",
   },
   {
     type: "Reel",
     href: "https://www.instagram.com/vtech.py/reel/DQaIv_4j_zN/",
-    image: "https://images.unsplash.com/photo-1584820927498-cfe5211fd8bf?w=500&q=80",
+    image: "dist/assets/image4_post.jpg",
+  },
+  {
+    type: "Reel",
+    href: "https://www.instagram.com/vtech.py/reel/DUZOieWD8Vl/",
+    image: "dist/assets/image3_post.jpg",
   },
   {
     type: "Reel",
     href: "https://www.instagram.com/vtech.py/reel/DPhlYZ4DzM4/",
-    image: "https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=500&q=80",
+    image: "dist/assets/image5_post.jpg",
   },
 ];
